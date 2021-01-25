3f0a468510bf55d047789b2f1c1e7960f9aaa0c0
0909oje <0909oje@naver.com>
2021-01-26 01:05:32 +0900

    activity_detail.xml 틀 짜기

--- app/src/main/java/com/example/pet_calendar/activity_detail.java
+++ app/src/main/java/com/example/pet_calendar/activity_detail.java
@@ -3,12 +3,14 @@ package com.example.pet_calendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class activity_detail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_detail);
     }
 }
