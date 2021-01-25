a0f78fed43fead1f42deb2cb1b34227dfdc2ad19
0909oje <0909oje@naver.com>
2021-01-26 00:20:12 +0900

    계획표 내 xml 전부 추가

---
+++ app/src/main/java/com/example/pet_calendar/activity_pet.java
@@ -0,0 +1,14 @@
+package com.example.pet_calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_pet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet);
+    }
+}
---
+++ app/src/main/java/com/example/pet_calendar/popup_pet_list.java
@@ -0,0 +1,14 @@
+package com.example.pet_calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class popup_pet_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_pet_list);
+    }
+}
