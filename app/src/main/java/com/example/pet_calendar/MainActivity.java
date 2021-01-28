57d6583f68c5a4e0648551ab91de84461aa796a5
0909oje <0909oje@naver.com>
2021-01-28 22:34:49 +0900

    레이아웃 수정

--- app/src/main/java/com/example/pet_calendar/MainActivity.java
+++ app/src/main/java/com/example/pet_calendar/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.pet_calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
--- app/src/main/java/com/example/pet_calendar/activity_detail.java
+++ app/src/main/java/com/example/pet_calendar/activity_detail.java
@@ -1,16 +1,58 @@
 package com.example.pet_calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class activity_detail extends AppCompatActivity {
 
+//  back 클릭 시 메인화면
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_detail);
+
+        Button back = (Button) findViewById(R.id.activity_detail_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//edit 클릭 시 +버튼 보이기
+        Button edit = (Button) findViewById(R.id.activity_detail_edit);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button add1 = (Button) findViewById(R.id.activity_detail_picture_add);
+                Button add2 = (Button) findViewById(R.id.activity_detail_profile_add);
+                add1.setVisibility(v.VISIBLE);
+                add2.setVisibility(v.VISIBLE);
+            }
+        });
+    }
+
+//    날짜 선택하기
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day){
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this,"Date: "+dateMessage,Toast.LENGTH_SHORT).show();
     }
 }
